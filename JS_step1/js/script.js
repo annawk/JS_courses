19e3288a217bf024d0b9922719faf39e2b1a6001
annawk <chikamojito@gmail.com>
2021-10-01 20:34:17 +0300

    step_1

---
+++ JS_step1/js/script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 3;
+console.log(number);
+
+const obj = {
+    name: "Masha",
+    age: 24,
+}; */
+
+/* console.log(obj.age);
+
+let arr = ['plum.png', 'apple.jpg', 6];
+console.log(arr[2]);
+
+/* alert('hello'); */
+
+/* const result = confirm('Do you like coffee?');
+console.log(result); */ 
+
+/* const answer = [];
+
+answer[0] = prompt('сколько вам лет?', '');
+answer[1] = prompt('любите красный?', 'да');
+answer[2] = prompt('нравится синий?', '');
+
+document.write(answer); */
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Misha';
+
+alert(`Hello, ${user}!`); */
+
+/* const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените фильм?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените фильм?", "");
+
+personalMovieDB.movies[a] = b; // писать надо квадратные скобки, во избежания ошибок вывода в разных браузерах.
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); */
+
+/* let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr); */
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
