c02f7f1af8f4c928df79f5562306c04e508575e7
annawk <chikamojito@gmail.com>
2021-10-07 13:34:47 +0300

    add 2 and 3 way to solve the task2

--- JS_step1/js/script.js
+++ JS_step1/js/script.js
@@ -9,12 +9,12 @@ console.log(number);
 const obj = {
     name: "Masha",
     age: 24,
-}; */
+}; 
 
-/* console.log(obj.age);
+console.log(obj.age);  */
 
-let arr = ['plum.png', 'apple.jpg', 6];
-console.log(arr[2]);
+/* let arr = ['plum.png', 'apple.jpg', 6];
+console.log(arr[2]); */
 
 /* alert('hello'); */
 
@@ -66,7 +66,67 @@ decr--;
 console.log(incr);
 console.log(decr); */
 
-const isChecked = true,
+/* const isChecked = true,
       isClose = false;
 
-console.log(isChecked && isClose);
+console.log(isChecked && isClose); */
+
+/* if (4==9) {
+    console.log('ok');
+} else {
+    console.log('Error');
+} */
+
+/* const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 70) {
+    console.log('Много');
+} else {
+    console.log('ok');
+}
+
+const num = 50;
+
+(num === 50) ? console.log('ok') : console.log('Error'); */
+
+/* const num = 50;
+
+switch (num) {
+        case 49:
+            console.log('Неверно');
+            break;
+        case 100:
+            console.log('неверно');
+            break;
+        case 50:
+            console.log('Верное');
+            break;
+        default:
+            console.log('Не в этот раз');
+            break;
+} */
+
+/* let num = 50;
+
+while ( num < 55) {
+    console.log(num);
+    num++;
+}
+
+let num = 50;
+
+do {
+    console.log(num);
+    num++;
+} while ( num < 55);
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) 
+    console.log(i); 
+}
+ */
+
+let num = 1708 % 2;
+console.log(num);
